2ef3a0fd00216e945b7be125b38b9267a519cc5c
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-06-14 18:50:16 -0300

    class jogo / jogador

--- ConsoleCore/GameTOP/Program.cs
+++ ConsoleCore/GameTOP/Program.cs
@@ -6,21 +6,33 @@ namespace GameTOP
     {
         static void Main(string[] args)
         {
-            var jogo = new JogoFODA("Ronaldo");
+            var jogo = new JogoFODA(new Jogador("Ronaldo"));
             jogo.IniciarJogo();
         }
     }
 
-    class JogoFODA
-    {   
-        private readonly string _NomeJogador;
-        public JogoFODA(string nome)
+    public class Jogador
+    {
+        public readonly string _Nome;
+
+        public Jogador(string nome)
         {
-            _NomeJogador = nome;
+            _Nome = nome;
         }
-        public void IniciarJogo()
+
+        //Chuta
+        public void chuta()
         {
-            Console.Write($"{_NomeJogador} deu um passe");
+            Console.Write($"{_Nome} está chutando");
+        }
+        //Corre
+        public void corre()
+        {            Console.Write($"{_Nome} está correndo");
+        }
+        //Passe
+        public void passe()
+        {
+            Console.Write($"{_Nome} está passando");
         }
     }
 }
