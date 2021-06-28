3b9b558881dc2abf575e138a54a2df3954520f1d
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-06-28 18:15:46 -0300

    Entidades proagil domain

---
+++ dotnetcore/ProAgil.Domain/Palestrante.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace ProAgil.Domain
+{
+    public class Palestrante
+    {
+        public int Id {get; set;}
+        public int Nome {get; set;}
+        public int MiniCurriculo {get; set;}
+        public int ImagemURL {get; set;}
+        public int Telefone {get; set;}
+        public int Email {get; set;}
+
+        public List<RedeSocial> RedesSociais {get; set;}
+        public List<PalestranteEvento>  PalestranteEventos {get; set;}
+
+    }
+}
