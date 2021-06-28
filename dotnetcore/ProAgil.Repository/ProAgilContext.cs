622c3e1c995277f1e537d49055104c5c9984fa27
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-06-28 18:29:13 -0300

    dentro do Repository criamos a class ProAgilContext.cs

---
+++ dotnetcore/ProAgil.Repository/ProAgilContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ProAgil.Domain;
+
+namespace ProAgil.Repository
+{
+    public class ProAgilContext : DbContext
+    {
+        public ProAgilContext(DbContextOptions<ProAgilContext> options) : base (options) {}
+
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Palestrante> Palestrantes { get; set; }
+        public DbSet<PalestranteEvento> PaletranteEventos { get; set; }
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<RedeSocial> RedeSociais { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {   
+            modelBuilder.Entity<PalestranteEvento>()
+            .HasKey (PaletranteEventos => new { PE.EventoId, PE.PalestranteId });
+        }
+    }
+}
